d0fe2dbae6ca2fa38d8f8ac43fc8bc3dea00b0eb
jihanapriliani <11211046@student.itk.ac.id>
2024-05-05 23:07:14 +0800

    comment font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,8 +9,8 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="//fonts.bunny.net">
-        <link href="//fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link href="//fonts.googleapis.com/css?family=Poppins:400,500,700,800&display=swap" rel="stylesheet">
+        {{-- <link href="//fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="//fonts.googleapis.com/css?family=Poppins:400,500,700,800&display=swap" rel="stylesheet"> --}}
 
        
 
