bb04be974a40fde90071844d20a00054e5e50f04
jihanapriliani <11211046@student.itk.ac.id>
2024-05-12 16:21:45 +0800

    fix-bug: register view

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -8,7 +8,7 @@ import { Head, Link, useForm } from "@inertiajs/react";
 
 export default function Register() {
     const { data, setData, post, processing, errors, reset } = useForm({
-        name: "",
+        fullname: "",
         email: "",
         password: "",
         password_confirmation: "",
@@ -30,34 +30,48 @@ export default function Register() {
         <GuestLayout>
             <Head title="Register" />
 
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="name" value="Name" />
-
+            <form onSubmit={submit} className="max-w-5xl mx-auto my-20">
+                <div className="my-5">
+                    <label
+                        htmlFor="fullname"
+                        className="block mb-2 font-medium text-gray-700 dark:text-white text-3xl"
+                    >
+                        Fullname
+                    </label>
                     <TextInput
-                        id="name"
-                        name="name"
-                        value={data.name}
-                        className="mt-1 block w-full"
-                        autoComplete="name"
+                        type="text"
+                        id="fullname"
+                        name="fullname"
+                        value={data.fullname}
+                        autoComplete="username"
+                        placeholder="John Doe"
+                        className="h-[50px]  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        style={{ fontSize: "2rem" }}
                         isFocused={true}
-                        onChange={(e) => setData("name", e.target.value)}
+                        onChange={(e) => setData("fullname", e.target.value)}
                         required
                     />
 
-                    <InputError message={errors.name} className="mt-2" />
+                    <InputError message={errors.fullname} className="mt-2" />
                 </div>
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="email" value="Email" />
-
+                <div className="my-5">
+                    <label
+                        htmlFor="email"
+                        className="block mb-2 font-medium text-gray-700 dark:text-white text-3xl"
+                    >
+                        Email
+                    </label>
                     <TextInput
-                        id="email"
                         type="email"
+                        id="email"
                         name="email"
                         value={data.email}
-                        className="mt-1 block w-full"
                         autoComplete="username"
+                        className="h-[50px]  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="name@gmail.com"
+                        style={{ fontSize: "2rem" }}
+                        isFocused={true}
                         onChange={(e) => setData("email", e.target.value)}
                         required
                     />
@@ -65,16 +79,22 @@ export default function Register() {
                     <InputError message={errors.email} className="mt-2" />
                 </div>
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
+                <div className="mb-5">
+                    <label
+                        htmlFor="password"
+                        className="block mb-2 font-medium text-gray-700 dark:text-white text-3xl"
+                    >
+                        Password
+                    </label>
                     <TextInput
                         id="password"
                         type="password"
                         name="password"
+                        className="h-[50px]  bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="*****"
+                        style={{ fontSize: "2rem" }}
                         value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
+                        autoComplete="current-password"
                         onChange={(e) => setData("password", e.target.value)}
                         required
                     />
@@ -82,19 +102,22 @@ export default function Register() {
                     <InputError message={errors.password} className="mt-2" />
                 </div>
 
-                <div className="mt-4">
-                    <InputLabel
+                <div className="mb-5">
+                    <label
                         htmlFor="password_confirmation"
-                        value="Confirm Password"
-                    />
-
+                        className="block mb-2 font-medium text-gray-700 dark:text-white text-3xl"
+                    >
+                        Confirm Password
+                    </label>
                     <TextInput
                         id="password_confirmation"
                         type="password"
                         name="password_confirmation"
+                        className="h-[50px]  bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="*****"
+                        style={{ fontSize: "2rem" }}
                         value={data.password_confirmation}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
+                        autoComplete="current-password"
                         onChange={(e) =>
                             setData("password_confirmation", e.target.value)
                         }
@@ -110,14 +133,18 @@ export default function Register() {
                 <div className="flex items-center justify-end mt-4">
                     <Link
                         href={route("login")}
-                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        className="underline text-sm mr-5 text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
                         Already registered?
                     </Link>
 
-                    <PrimaryButton className="ms-4" disabled={processing}>
+                    <button
+                        type="submit"
+                        className="px-12 py-4 rounded-lg bg-black text-white text-3xl"
+                        disabled={processing}
+                    >
                         Register
-                    </PrimaryButton>
+                    </button>
                 </div>
             </form>
         </GuestLayout>
